de07a69cafae981a283f5dad14ed816f6bdf3f65
handwoong <handwoong@gmail.com>
2025-03-27 18:52:08 +0900

    MDHU-272 test: 디스코드 메시지 전송 테스트 이름 수정

--- src/test/kotlin/com/everyonewaiter/common/notification/discord/DiscordWebhookClientTest.kt
+++ src/test/kotlin/com/everyonewaiter/common/notification/discord/DiscordWebhookClientTest.kt
@@ -7,19 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest
 class DiscordWebhookClientTest(
     private val discordWebhookClient: DiscordWebhookClient,
 ) : FunSpec({
-        xtest("디스코드 웹훅 클라이언트를 이용해 메시지를 전송한다.") {
-            val payload = DiscordWebhookPayload(
-                listOf(
-                    DiscordEmbed(
-                        title = "제목",
-                        description = "설명",
-                        color = 16711680,
-                        fields = listOf(
-                            DiscordField("모두의 웨이터", "Hello World"),
+        xcontext("send") {
+            test("디스코드 메시지를 전송한다.") {
+                val payload = DiscordWebhookPayload(
+                    listOf(
+                        DiscordEmbed(
+                            title = "제목",
+                            description = "설명",
+                            color = 16711680,
+                            fields = listOf(
+                                DiscordField("모두의 웨이터", "Hello World"),
+                            ),
                         ),
                     ),
-                ),
-            )
-            discordWebhookClient.send("<DISCORD_WEBHOOK_URI>", payload)
+                )
+                discordWebhookClient.send("<DISCORD_WEBHOOK_URI>", payload)
+            }
         }
     })
