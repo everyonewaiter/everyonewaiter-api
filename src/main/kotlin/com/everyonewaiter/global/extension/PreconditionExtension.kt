40c6355948ad98098889b22e19efd43045b30845
handwoong <handwoong@gmail.com>
2025-04-02 02:20:03 +0900

    MDHU-276 feat: 컨디션 체크 스마트 캐스팅 설정

--- src/main/kotlin/com/everyonewaiter/global/extension/PreconditionExtension.kt
+++ src/main/kotlin/com/everyonewaiter/global/extension/PreconditionExtension.kt
@@ -2,11 +2,17 @@ package com.everyonewaiter.global.extension
 
 import com.everyonewaiter.global.exception.BusinessException
 import com.everyonewaiter.global.exception.ErrorCode
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
+@OptIn(ExperimentalContracts::class)
 fun checkOrThrow(
     value: Boolean,
     errorCode: ErrorCode,
 ) {
+    contract {
+        returns() implies value
+    }
     if (!value) {
         throw BusinessException(errorCode)
     }
