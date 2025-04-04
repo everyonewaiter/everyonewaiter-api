357f73c4a31b607a9ead32448e93c2e9841f8e19
handwoong <handwoong@gmail.com>
2025-04-05 00:59:58 +0900

    SKEU-7 feat: 오라클 오브젝트 스토리지 클라이언트 사용 시 생성하도록 변경

--- src/test/kotlin/com/everyonewaiter/infrastructure/image/OracleObjectStorageClientTest.kt
+++ src/test/kotlin/com/everyonewaiter/infrastructure/image/OracleObjectStorageClientTest.kt
@@ -3,29 +3,18 @@ package com.everyonewaiter.infrastructure.image
 import com.everyonewaiter.common.file.SimpleMultipartFile
 import com.everyonewaiter.global.exception.BusinessException
 import com.everyonewaiter.global.exception.ErrorCode
-import com.oracle.bmc.objectstorage.ObjectStorageClient
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.mockk
 
 class OracleObjectStorageClientTest :
     FunSpec({
-        val objectStorageClient = mockk<ObjectStorageClient>(relaxed = true)
         val oracleObjectStorageClient = OracleObjectStorageClient(
             namespace = "namespace",
             bucketName = "bucketName",
-            objectStorageClient = objectStorageClient,
         )
 
         context("upload") {
-            test("이미지 업로드에 성공하면 파일명을 반환한다.") {
-                val name = "license/202501/test.webp"
-                val file = SimpleMultipartFile("abc.jpg", ByteArray(0), name, "image/webp")
-                val actual = oracleObjectStorageClient.upload(file)
-                actual shouldBe file.originalFilename
-            }
-
             test("파일명을 찾을 수 없다면 예외가 발생한다.") {
                 val file = SimpleMultipartFile("", ByteArray(0))
                 shouldThrow<BusinessException> {
