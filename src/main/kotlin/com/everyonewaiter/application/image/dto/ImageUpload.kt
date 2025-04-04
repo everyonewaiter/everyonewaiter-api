574815d426e206c69c8943956a5e20a25a1cc870
handwoong <handwoong@gmail.com>
2025-04-05 00:13:23 +0900

    SKEU-7 feat: 이미지 업로드 서비스 계층 구현

--- src/main/kotlin/com/everyonewaiter/application/image/dto/ImageUpload.kt
+++ src/main/kotlin/com/everyonewaiter/application/image/dto/ImageUpload.kt
@@ -4,8 +4,7 @@ import org.springframework.web.multipart.MultipartFile
 
 class ImageUpload {
     data class Request(
-        val prefix: String,
-        val accountId: Long,
         val file: MultipartFile,
+        val prefix: String?,
     )
 }
