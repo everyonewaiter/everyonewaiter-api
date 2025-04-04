6fc83bddb1a26c3d0e3626066915331e33471389
handwoong <handwoong@gmail.com>
2025-04-05 04:49:04 +0900

    SKEU-7 feat: 로컬 환경 매장 등록 더미 데이터 생성

--- src/main/kotlin/com/everyonewaiter/support/LocalEnvironmentDataInitializer.kt
+++ src/main/kotlin/com/everyonewaiter/support/LocalEnvironmentDataInitializer.kt
@@ -2,9 +2,13 @@ package com.everyonewaiter.support
 
 import com.everyonewaiter.common.tsid.Tsid
 import com.everyonewaiter.domain.account.entity.Account
+import com.everyonewaiter.domain.account.repository.AccountRepository
+import com.everyonewaiter.domain.store.entity.BusinessLicenseInformation
+import com.everyonewaiter.domain.store.entity.StoreRegistration
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Profile
 import org.springframework.data.jdbc.core.JdbcAggregateOperations
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import org.springframework.transaction.support.TransactionTemplate
 import java.util.concurrent.CountDownLatch
@@ -15,9 +19,27 @@ import java.util.concurrent.Executors
 class LocalEnvironmentDataInitializer(
     private val transactionTemplate: TransactionTemplate,
     private val jdbcAggregateOperations: JdbcAggregateOperations,
+    private val passwordEncoder: PasswordEncoder,
+    private val accountRepository: AccountRepository,
 ) : CommandLineRunner {
     override fun run(vararg args: String?) {
+        val adminAccountId = getAdminAccountId()
         initAccounts()
+        initStoreRegistrations(adminAccountId)
+    }
+
+    private fun getAdminAccountId(): Long {
+        val adminAccount = accountRepository.findByEmail("admin@everyonewaiter.com")
+        return adminAccount?.id
+            ?: accountRepository
+                .save(
+                    Account(
+                        id = Tsid.nextLong(),
+                        email = "admin@everyonewaiter.com",
+                        password = passwordEncoder.encode("@password1"),
+                        phoneNumber = "01044591812",
+                    ),
+                ).id
     }
 
     private fun initAccounts() {
@@ -38,6 +60,33 @@ class LocalEnvironmentDataInitializer(
                 email = "${Tsid.nextString()}@everyonewaiter.com",
                 password = "@password1",
                 phoneNumber = Tsid.nextLong().toString().takeLast(11),
+            )
+        }
+
+    private fun initStoreRegistrations(adminAccountId: Long) {
+        val storeRegistrationCount = jdbcAggregateOperations.count(StoreRegistration::class.java)
+        if (storeRegistrationCount > 0) return
+        BulkInsertExecutor(
+            executorName = "StoreRegistration",
+            transactionTemplate = transactionTemplate,
+            jdbcAggregateOperations = jdbcAggregateOperations,
+            generateData = { generateStoreRegistrations(adminAccountId) },
+        ).execute()
+    }
+
+    private fun generateStoreRegistrations(adminAccountId: Long) =
+        List(BULK_INSERT_SIZE) {
+            StoreRegistration(
+                id = Tsid.nextLong(),
+                accountId = adminAccountId,
+                licenseInformation = BusinessLicenseInformation(
+                    name = "홍길동식당",
+                    ceoName = "홍길동",
+                    address = "경상남도 창원시 의창구 123",
+                    landline = "055-123-4567",
+                    license = "123-45-67890",
+                    image = "license/202504/0KA652ZFZ26DG.webp",
+                ),
             )
         }
 
@@ -65,7 +114,7 @@ class LocalEnvironmentDataInitializer(
         }
     }
 
-    companion object {
+    private companion object {
         private const val BULK_INSERT_SIZE = 1000
         private const val EXECUTE_COUNT = 10000
     }
