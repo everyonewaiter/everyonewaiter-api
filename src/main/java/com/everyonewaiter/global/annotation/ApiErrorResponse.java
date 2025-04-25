987afc28de285f3a1a49e64f458a2e33f84ed06e
handwoong <handwoong@gmail.com>
2025-04-25 22:38:30 +0900

    SKEU-1: API 문서 Swagger 에러 응답 어노테이션 정의

---
+++ src/main/java/com/everyonewaiter/global/annotation/ApiErrorResponse.java
@@ -0,0 +1,22 @@
+package com.everyonewaiter.global.annotation;
+
+import com.everyonewaiter.global.exception.ErrorCode;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.http.MediaType;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiErrorResponse {
+
+  String summary() default "";
+
+  ErrorCode code();
+
+  String exampleName() default "";
+
+  String mediaType() default MediaType.APPLICATION_JSON_VALUE;
+
+}
