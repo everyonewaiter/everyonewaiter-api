5abe7154a95a6baf8287061f40fe9e6c9791678c
handwoong <handwoong@gmail.com>
2025-04-27 00:38:40 +0900

    SKEU-1: 날짜 포매터 캐싱

--- src/main/java/com/everyonewaiter/global/config/InstantJsonSerializer.java
+++ src/main/java/com/everyonewaiter/global/config/InstantJsonSerializer.java
@@ -1,18 +1,17 @@
 package com.everyonewaiter.global.config;
 
+import com.everyonewaiter.global.domain.support.DateFormatter;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import org.springframework.boot.jackson.JsonComponent;
 
 @JsonComponent
 class InstantJsonSerializer extends JsonSerializer<Instant> {
 
-  private static final String SERIALIZE_FORMAT = "yyyy-MM-dd HH:mm:ss";
   private static final String ZONE_ID = "Asia/Seoul";
 
   @Override
@@ -22,7 +21,7 @@ class InstantJsonSerializer extends JsonSerializer<Instant> {
       SerializerProvider serializerProvider
   ) throws IOException {
     jsonGenerator.writeString(
-        DateTimeFormatter.ofPattern(SERIALIZE_FORMAT)
+        DateFormatter.SERIALIZE_FORMATTER
             .withZone(ZoneId.of(ZONE_ID))
             .format(instant)
     );
---
+++ src/main/java/com/everyonewaiter/global/domain/support/DateFormatter.java
@@ -0,0 +1,13 @@
+package com.everyonewaiter.global.domain.support;
+
+import java.time.format.DateTimeFormatter;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DateFormatter {
+
+  public static final DateTimeFormatter SERIALIZE_FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+}
