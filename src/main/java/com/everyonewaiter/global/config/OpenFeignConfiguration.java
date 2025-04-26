a9d13e18b08faf6f4ec116448f5e5266a9fd9f99
handwoong <handwoong@gmail.com>
2025-04-26 16:34:18 +0900

    SKEU-1: Open Feign 설정

---
+++ src/main/java/com/everyonewaiter/global/config/OpenFeignConfiguration.java
@@ -0,0 +1,17 @@
+package com.everyonewaiter.global.config;
+
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.everyonewaiter.infrastructure")
+class OpenFeignConfiguration {
+
+  @Bean
+  public Logger.Level feignLoggerLevel() {
+    return Logger.Level.FULL;
+  }
+
+}
