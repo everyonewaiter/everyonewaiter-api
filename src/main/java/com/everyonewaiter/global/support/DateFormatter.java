953dc23c23319f3dba5514c4bd55ec06308f19d0
handwoong <handwoong@gmail.com>
2025-04-29 15:01:17 +0900

    SKEU-7: 파일명에 사용할 날짜 포매터 추가

--- src/main/java/com/everyonewaiter/global/support/DateFormatter.java
+++ src/main/java/com/everyonewaiter/global/support/DateFormatter.java
@@ -9,5 +9,6 @@ public final class DateFormatter {
 
   public static final DateTimeFormatter SERIALIZE =
       DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+  public static final DateTimeFormatter YEAR_MONTH = DateTimeFormatter.ofPattern("yyyyMM");
 
 }
