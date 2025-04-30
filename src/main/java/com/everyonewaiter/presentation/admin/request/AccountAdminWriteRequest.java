16e9350f4b972b75f983e9f14db20cbf79c79ea8
handwoong <handwoong@gmail.com>
2025-05-01 00:04:54 +0900

    SKEU-31: 불필요한 Enum 유효성 검사 기능 제거

--- src/main/java/com/everyonewaiter/presentation/admin/request/AccountAdminWriteRequest.java
+++ src/main/java/com/everyonewaiter/presentation/admin/request/AccountAdminWriteRequest.java
@@ -2,7 +2,6 @@ package com.everyonewaiter.presentation.admin.request;
 
 import com.everyonewaiter.application.account.request.AccountAdminWrite;
 import com.everyonewaiter.domain.account.entity.Account;
-import com.everyonewaiter.global.annotation.Enum;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -15,12 +14,10 @@ public class AccountAdminWriteRequest {
   public record Update(
       @Schema(description = "상태", example = "ACTIVE")
       @NotNull(message = "계정 상태가 누락되었거나 옳바르지 않습니다.")
-      @Enum(clazz = Account.State.class, message = "계정 상태가 누락되었거나 옳바르지 않습니다.")
       Account.State state,
 
       @Schema(description = "권한", example = "ADMIN")
       @NotNull(message = "계정 권한이 누락되었거나 옳바르지 않습니다.")
-      @Enum(clazz = Account.Permission.class, message = "계정 권한이 누락되었거나 옳바르지 않습니다.")
       Account.Permission permission
   ) {
 
