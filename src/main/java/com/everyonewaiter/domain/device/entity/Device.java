cd8cb7dcb7fb154dac9a31e85dc72a0961e0e8af
handwoong <handwoong@gmail.com>
2025-05-01 11:48:48 +0900

    SKEU-40: 기기 도메인 엔티티 정의

---
+++ src/main/java/com/everyonewaiter/domain/device/entity/Device.java
@@ -0,0 +1,64 @@
+package com.everyonewaiter.domain.device.entity;
+
+import com.everyonewaiter.global.domain.entity.AggregateRoot;
+import com.everyonewaiter.global.support.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Table(
+    name = "device",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_device_store_id_name",
+            columnNames = {"store_id", "name"}
+        ),
+    }
+)
+@Entity
+@Getter
+@ToString(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Device extends AggregateRoot<Device> {
+
+  public enum Purpose {POS, HALL, TABLE, WAITING}
+
+  public enum State {INACTIVE, ACTIVE}
+
+  public enum PaymentType {PREPAID, POSTPAID}
+
+  @Column(name = "store_id", nullable = false, updatable = false)
+  private Long storeId;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "purpose", nullable = false)
+  private Purpose purpose;
+
+  @Column(name = "table_no", nullable = false)
+  private int tableNo;
+
+  @Column(name = "ksnet_device_no", nullable = false)
+  private String ksnetDeviceNo = "";
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "state", nullable = false)
+  private State state = State.ACTIVE;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "payment_type", nullable = false)
+  private PaymentType paymentType = PaymentType.POSTPAID;
+
+  @Column(name = "secret_key", nullable = false)
+  private String secretKey = Tsid.nextString();
+
+}
