e5e80a9a3f447192b7a067986fe73fd3adaaa678
handwoong <handwoong@gmail.com>
2025-05-12 20:30:41 +0900

    SKEU-70: 레디스 커넥션 팩토리 자동 구성 사용

--- src/main/java/com/everyonewaiter/global/config/CacheConfiguration.java
+++ src/main/java/com/everyonewaiter/global/config/CacheConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,7 +18,7 @@ class CacheConfiguration {
 
   @Bean
   public CacheManager cacheManager(
-      RedisConnectionFactory connectionFactory,
+      LettuceConnectionFactory connectionFactory,
       RedisCacheConfiguration redisCacheConfiguration
   ) {
     return RedisCacheManager.RedisCacheManagerBuilder
--- src/main/java/com/everyonewaiter/global/config/RedisConfiguration.java
+++ src/main/java/com/everyonewaiter/global/config/RedisConfiguration.java
@@ -2,7 +2,6 @@ package com.everyonewaiter.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,12 +10,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 class RedisConfiguration {
 
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
-  }
-
-  @Bean
-  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+  public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
     StringRedisTemplate redisTemplate = new StringRedisTemplate(connectionFactory);
     redisTemplate.setEnableTransactionSupport(true);
     return redisTemplate;
