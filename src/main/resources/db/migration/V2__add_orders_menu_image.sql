b341d549cf55c5c123de535976c51836dea6dc6d
handwoong <handwoong@gmail.com>
2025-09-28 16:30:20 +0900

    SKEU-350: 주문 메뉴 이미지 표시 여부 설정 추가

--- src/main/resources/db/migration/V2__add_orders_menu_image.sql
+++ src/main/resources/db/migration/V2__add_orders_menu_image.sql
@@ -1,2 +1,5 @@
+alter table store_setting
+    add column show_order_menu_image boolean not null default true;
+
 alter table orders_menu
     add column image char(30) not null default 'menu/preparation.png';
