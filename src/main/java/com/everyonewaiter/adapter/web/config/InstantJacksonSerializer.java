2681f24fe0d7654f9a8a7315fe348d359664e5e6
handwoong <handwoong@gmail.com>
2026-01-05 23:11:31 +0900

    fix: SonarQube 경고 수정

--- src/main/java/com/everyonewaiter/adapter/web/config/InstantJacksonSerializer.java
+++ src/main/java/com/everyonewaiter/adapter/web/config/InstantJacksonSerializer.java
@@ -1,9 +1,11 @@
 package com.everyonewaiter.adapter.web.config;
 
 import static com.everyonewaiter.domain.support.TimeZone.ASIA_SEOUL;
+import static lombok.AccessLevel.PRIVATE;
 
 import com.everyonewaiter.domain.support.DateFormatter;
 import java.time.Instant;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.jackson.JacksonComponent;
 import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonGenerator;
@@ -11,8 +13,10 @@ import tools.jackson.databind.SerializationContext;
 import tools.jackson.databind.ValueSerializer;
 
 @JacksonComponent
+@NoArgsConstructor(access = PRIVATE)
 class InstantJacksonSerializer {
 
+  @SuppressWarnings("unused")
   static class Serializer extends ValueSerializer<Instant> {
 
     @Override
